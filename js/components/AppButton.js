67c4d53d281dc6c3d796ba85ce5e8c0f3daa0038
aamir <maamir.se.uol@gmail.com>
2022-10-07 17:23:25 +0500

    props in vue

--- js/components/AppButton.js
+++ js/components/AppButton.js
@@ -1,14 +1,24 @@
-export default {
+export default{
     template:
     // ` <button class="btn-dark rounded">Hi there</button> `
     // slot 
-    ` <button class="btn-dark rounded" :disabled="processing">
+    ` <button 
+        :class="{
+            'btn-dark rounded': true
+        }" 
+        :disabled="processing">
         <slot />
     </button> `
 
     // slot end 
 
-        ,
+    ,
+    props:{
+        type: {
+            type: String,
+            default: 'primary'
+        }
+    },
     data(){
         return {
             processing: true
