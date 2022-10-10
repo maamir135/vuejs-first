d0f70c93f14fd3f9feeb237c08fbacfacfec8a08
aamir <maamir.se.uol@gmail.com>
2022-10-10 12:42:06 +0500

    bring it all together

--- js/components/AssignmentList.js
+++ js/components/AssignmentList.js
@@ -1,29 +1,14 @@
+import Assignment from "./Assignment.js";
 export default{
-    template: ` 
-        <section class="p-5" v-show="assignments.length">
+
+    components: {Assignment},
+    template: `  
+        <section class="p-3" v-show="assignments.length">
             <h2>{{title}}</h2>
             <ul>
-
-                <li v-for="assignment in assignments" :key="assignment.id">
-                    <label for="">
-                        {{ assignment.name }}
-                        <input type="checkbox" v-model="assignment.complete">
-                    </label>
-                    
-                </li>
-            </ul>
-        </section>
-        <section class="p-5"  v-if="completedAssignment.length">
-            <h2>Completed</h2>
-
-            <ul>
-                <li v-for="assignment in completedAssignment">
-                    <label for="">
-                        {{ assignment.name }}
-                        <input type="checkbox" v-model="assignment.complete" :key="assignment.id">
-                    </label>
-                    
-                </li>
+                <assignment 
+                    v-for="assignment in assignments" :key="assignment.id" :assignment="assignment">
+                </assignment>
             </ul>
         </section>
     `,
--- js/components/Assignments.js
+++ js/components/Assignments.js
@@ -2,23 +2,12 @@ import AssignmentList from "./AssignmentList.js";
 
 export default {
     components: {AssignmentList},
-    template:`<assignment-list :assignments="inprogressAssignment" title="In Progress"> </assignment-list>
-
-            <section class="p-5"  v-if="completedAssignment.length">
-                <h2>Completed</h2>
-
-                <ul>
-                    <li v-for="assignment in completedAssignment">
-                        <label for="">
-                            {{ assignment.name }}
-                            <input type="checkbox" v-model="assignment.complete" :key="assignment.id">
-                        </label>
-                        
-                    </li>
-                </ul>
+    template:`\
+            <section>
+                <assignment-list :assignments="filters.inProgress" title="In Progress"> </assignment-list>
+                <assignment-list :assignments="filters.completed" title="Completed"> </assignment-list>
             </section>
-
-    `,
+            `,
     
             data(){
                 return {
@@ -30,13 +19,14 @@ export default {
                     ]
                 }
             },
+
             computed:{
-                completedAssignment(){
-                    return this.assignments.filter(assignment => assignment.complete);
-                },
-                inprogressAssignment(){
-                    return this.assignments.filter(assignment => !assignment.complete);
-                    
+                filters(){
+                    return {
+                        inProgress: this.assignments.filter(assignment => !assignment.complete),
+                        completed: this.assignments.filter(assignment => assignment.complete),
+
+                    };
                 }
             }
 
